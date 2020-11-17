2c603ab02eb39fab6e66086dcda0f6ac0f6f1164
rjb-dev <rebeccablundell@catalyst.net.nz>
2020-11-17 19:41:17 +1300

    Update readme

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Kia ora koutou,")
+print("")
+print("This is a simple python program.")
+print("What does it do? Just prints out this message.")
+print("")
+print("Ngā mihi, Evan & Rebecca")
+print("")
+print("PS. I'm learning Git")
